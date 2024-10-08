2bf1485074021149c6f82fda7e89da106d540346
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-08 16:06:40 +0600

    Created API and Slices for Authentication

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
@@ -6,5 +6,5 @@ export const apiSlice = createApi({
     baseUrl: process.env.REACT_APP_API_URL,
   }),
   tagTypes: [],
-  endpoints: () => ({}),
+  endpoints: (builder) => ({}),
 });
