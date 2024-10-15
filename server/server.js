7d25c5a6e709c590d7b8ff1a0abb8c40477870a3
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-15 09:38:36 +0600

    Emitting Events from server

--- server/server.js
+++ server/server.js
@@ -10,6 +10,28 @@ const io = require("socket.io")(server);
 global.io = io;
 
 const router = jsonServer.router("db.json");
+
+router.render = (req, res) => {
+  const path = req.path;
+  const method = req.method;
+
+  if (
+    path.includes("/conversations") &&
+    (method === "POST" || method === "PATCH")
+  ) {
+    io.emit("conversation", {
+      data: res.locals.data,
+    });
+  }
+
+//   if (path.includes("/messages") && method === "POST") {
+//     io.emit("message", {
+//       data: res.locals.data,
+//     });
+//   }
+  res.json(res.locals.date);
+};
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
@@ -19,9 +41,9 @@ app.db = router.db;
 app.use(middlewares);
 
 const rules = auth.rewriter({
-    users: 640,
-    conversations: 660,
-    messages: 660,
+  users: 640,
+  conversations: 660,
+  messages: 660,
 });
 
 app.use(rules);
